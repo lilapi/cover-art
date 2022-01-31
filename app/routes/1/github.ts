36e3c5bb256b7bf52926ba6f62af4c7600f53a1f
Patrick Smith <pgwsmith@gmail.com>
2022-01-31 11:45:08 +1100

    Remove unused parameter

--- app/routes/1/github.ts
+++ app/routes/1/github.ts
@@ -1,13 +1,19 @@
 import type { LoaderFunction } from "remix";
 import { renderContent } from "~/graphics/render";
 import { readParams } from "~/primitives/params";
-import { resPNGImage } from "~/responses";
+import { resJPEGImage, resPNGImage } from "~/responses";
 import { githubTemplate } from "~/templates/github";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url, "https://example.org");
   const params = readParams(url.searchParams)
   const options = await githubTemplate(params);
-  const pngData = await renderContent(options);
-  return resPNGImage(pngData);
+  const format = params.string("format", "png");
+  if (format === "jpeg") {
+    const pngData = await renderContent(options);
+    return resJPEGImage(jpegData);
+  } else {
+    const pngData = await renderContent(options);
+    return resPNGImage(pngData);
+  }
 };
